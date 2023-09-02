d61750c0cf78e4cb7be065ac5c18f949eea89433
Kingshuk Roy <kingshukroy98@gmail.com>
2023-09-02 15:12:56 +0900

    Develop getFood

---
+++ src/food/foodController.ts
@@ -0,0 +1,12 @@
+import {Request, Response} from 'express';
+import foodModel from "./foodModel"
+
+export async function getFood(req: Request, res: Response) {
+    try {
+        const food = await foodModel.getFood();
+        res.status(200).send(food)
+    }
+    catch (error) {
+        res.status(500).send("Error: " + error)
+    }
+}
