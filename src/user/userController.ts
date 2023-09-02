325cd4499c71b34f434321e9d5862ff3382967a2
Kingshuk Roy <kingshukroy98@gmail.com>
2023-09-02 11:59:49 +0900

    Add logout function

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,15 @@
 import {Request, Response} from 'express';
 
-
+export async function logout(req: Request, res: Response) {
+    req.session.destroy((error: any) => {
+        if (error) {
+            res.status(500).send("Error: " + error)
+        }
+        else {
+            res.status(200).send("Logging out");
+        }
+    });
+}
 
 
 
