ce90c58e6fd112a53e6ba3aaf4f3da8331b260b2
Kingshuk Roy <kingshukroy98@gmail.com>
2023-09-02 14:04:14 +0900

    Develop deleteUserById and leave comment for future development changes of two latest functions

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -48,6 +48,11 @@ export async function createUser(req: Request, res: Response) {
     }
 }
 
+//When thinking about these next two functions I realized they will be more complex (needing to be logged in with 
+// supabase first before approval to udpate then specifying what of the user account to update, which for us would just be the user's kitchens)
+// We would need the session id or the token from supabase to see the user is logged in first before being able to update and delete
+// For MVP, these functionalities doon't have to be super fleshed out, these are just basic things we can use for testing
+
 export async function updateUserById(req: Request, res: Response) {
     try {
         const id = req.params.id;
@@ -56,6 +61,17 @@ export async function updateUserById(req: Request, res: Response) {
     }
     catch (error) {
         res.status(500).send("Error: " + error);
+    }
+}
+
+export async function deleteUserById(req: Request, res: Response) {
+    try {
+        const id = req.params.id;
+        await userModel.deleteUserById(id);
+        res.status(200).send("User deleted");
+    }
+    catch (error) {
+        res.status(500).send("Error: " + error)
     }
 }
     
