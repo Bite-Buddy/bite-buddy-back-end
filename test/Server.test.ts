25c4557468063c001bb1eb8667934b203cb5961c
Emi <emi.hokama.eckstein@gmail.com>
2023-09-02 23:51:17 +0900

    Add '/.kitchenId' endpoint test

--- test/Server.test.ts
+++ test/Server.test.ts
@@ -11,3 +11,27 @@ describe("/", () => {
             });
     });
 })
+
+describe("/:kitchenId", () => {
+    describe("/#GET", () => {
+        test("Should exist")
+    });
+    describe("/#DELETE", () => { });
+
+    /**For food */
+    describe("/:kitchenId/:ingredientId", () => {
+        describe("/#GET", () => {
+            test.todo("Should get the food item with given ingredientId")
+        });
+        describe("/#PATCH", () => {
+            test.todo("Should update the food item with given ingredientID and value");
+            test.todo("Should return error if parameter is not given");
+            test.todo("Should return error if ")
+        });
+    });
+
+    describe("/:kitchenId/add", () => {
+        describe("/#POST", () => { test.todo("Should add new kitchen") });
+    });
+});
+
