6383ea395de43324b77a8d64ec4f54f5774a6c71
izabelakucharska <karwowskaizabela0@gmail.com>
2023-09-02 18:59:42 +0900

    Add createFood function

--- src/food/foodModel.ts
+++ src/food/foodModel.ts
@@ -1,5 +1,18 @@
 import prisma from '../util/prisma-client';
 
+export async function createFood(kitchenId: number, foodName: string) {
+  return  await prisma.food.create({
+    data: {
+      name: foodName,
+      kitchen: {
+        create: [
+          { kitchen: { connect: { id: kitchenId } } },
+        ],
+      },
+    },
+  });
+}
+
 export async function getFoodById(foodId: number) {
   return  await prisma.food.findUnique({
     where: {id: foodId}
