1c584b94bcc951f80a822402cdb7d3f27a0d1e60
izabelakucharska <karwowskaizabela0@gmail.com>
2023-09-02 18:19:03 +0900

    Add kitchen model and createKitchen function

---
+++ src/kitchen/kitchenModel.ts
@@ -0,0 +1,14 @@
+import prisma from '../util/prisma-client';
+
+
+export async function createKitchen(userId: number) {
+  return  await prisma.kitchen.create({
+    data: {
+      users: {
+        create: [
+          { user: { connect: { id: userId } } },
+        ],
+      },
+    },
+  });
+}
