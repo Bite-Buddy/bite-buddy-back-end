aacc65ab803fda199bc160c0c8e6dd24d98bc590
Kingshuk Roy <kingshukroy98@gmail.com>
2023-09-02 15:07:20 +0900

    Edit server endpoints to start with /

--- server.ts
+++ server.ts
@@ -20,23 +20,23 @@ const app = express();
   app.get("/logout", userController.logout);
   app.get("/users", userController.getUsers); // mainly here for testing
   app.get("/user/:id", userController.getById);
-  app.post("user", userController.createUser);
-  app.patch("user/:id", userController.updateUserById); // probably need more patch endpoints
-  app.delete("user/:id", userController.deleteUserById);
+  app.post("/user", userController.createUser);
+  app.patch("/user/:id", userController.updateUserById); // probably need more patch endpoints
+  app.delete("/user/:id", userController.deleteUserById);
 
   // kitchen
   app.get("/kitchens", kitchenController.getKitchens); // mainly here for testing
   app.get("/kitchen/:id", kitchenController.getById);
   app.get("/kitchen/:id/users", kitchenController.getUsersByKitchen); // get all related users
-  app.post("/kitchen", kitchenController.createKitchen);
-  app.delete("/kitchen/:id", kitchenController.deleteKitchen);
+  app.post("/user/kitchen", kitchenController.createKitchen);
+  app.delete("/user/kitchen/:id", kitchenController.deleteKitchen);
 
   // food
   app.get("/food", foodController.getFood);
   app.get("/food/:id", foodController.getFoodById);
-  app.post("food", foodController.createFood);
-  app.patch("food/:id", foodController.updateFoodById);
-  app.delete("food/:id", foodController.deleteFoodById);
+  app.post("/food", foodController.createFood);
+  app.patch("/food/:id", foodController.updateFoodById);
+  app.delete("/food/:id", foodController.deleteFoodById);
 
   return app;
 }
