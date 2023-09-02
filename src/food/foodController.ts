13f8b5805ede918cfc35bd75d490dff15d60d477
Kingshuk Roy <kingshukroy98@gmail.com>
2023-09-02 15:16:16 +0900

    Develop getFoodById

--- src/food/foodController.ts
+++ src/food/foodController.ts
@@ -10,3 +10,14 @@ export async function getFood(req: Request, res: Response) {
         res.status(500).send("Error: " + error)
     }
 }
+
+export async function getFoodById(req: Request, res: Response) {
+    try {
+        const foodId = req.params.id;
+        const food = await foodModel.getFoodById(foodId);
+        res.status(200).send(food)
+    }
+    catch (error) {
+        res.status(500).send("Error: " + error)
+    }
+}
