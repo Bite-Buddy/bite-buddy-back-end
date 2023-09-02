5fd942c17a1c48ad86faf3c7ea661a2693fe4f72
izabelakucharska <karwowskaizabela0@gmail.com>
2023-09-02 17:56:26 +0900

    Add user model and prisma cilent file to util

---
+++ src/user/userModel.ts
@@ -0,0 +1,25 @@
+import prisma from '../util/prisma-client';
+
+interface IUser {
+  email: string,
+}
+
+export async function createNewUser(user: IUser) {
+  return await prisma.user.create({
+    data: user
+  })
+}
+
+export async function getUsers() {
+  return await prisma.user.create({
+    data: user
+  })
+}
+
+export async function findUser(email: string) {
+  return await prisma.user.findUnique({
+    where: {
+      email: email
+    }
+  })
+}
