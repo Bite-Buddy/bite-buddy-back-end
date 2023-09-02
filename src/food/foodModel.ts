afe90a40d1704cd4e81e72eae14c16839a77e783
izabelakucharska <karwowskaizabela0@gmail.com>
2023-09-02 18:52:02 +0900

    Add getFoodById function

--- src/food/foodModel.ts
+++ src/food/foodModel.ts
@@ -1,5 +1,10 @@
 import prisma from '../util/prisma-client';
 
+export async function getFoodById(foodId: number) {
+  return  await prisma.food.findUnique({
+    where: {id: foodId}
+  });
+}
 export async function getFood() {
   return  await prisma.food.findMany();
 }
