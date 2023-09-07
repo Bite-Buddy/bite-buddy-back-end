15422efd07f924f335174338f630040b6b4953b1
Emi <emi.hokama.eckstein@gmail.com>
2023-09-08 00:24:21 +0900

    Add 'boughtOn' date handling

--- src/food/foodModel.ts
+++ src/food/foodModel.ts
@@ -6,21 +6,22 @@ interface IFoodUpdate {
   updated_on?: Date
 }
 
-export async function createFood(kitchenId: number, foodName: string) {
-  return  await prisma.food.create({
+export async function createFood(kitchenId: number, foodName: string, boughtOn: Date) {
+  return await prisma.food.create({
     data: {
       name: foodName,
+      bought_on: boughtOn,
       kitchen: {
-        create: [
-          { kitchen: { connect: { id: kitchenId } } },
-        ],
-      },
-    },
+        connect: {
+          id: kitchenId
+        }
+      }
+    }
   });
 }
 
 export async function updateFoodById(foodId: number, foodUpdate: IFoodUpdate) {
-  return  await prisma.food.update({
+  return await prisma.food.update({
     where: {
       id: foodId
     },
@@ -29,17 +30,17 @@ export async function updateFoodById(foodId: number, foodUpdate: IFoodUpdate) {
 }
 
 export async function getFoodById(foodId: number) {
-  return  await prisma.food.findUnique({
-    where: {id: foodId}
+  return await prisma.food.findUnique({
+    where: { id: foodId }
   });
 }
 
 export async function getFood() {
-  return  await prisma.food.findMany();
+  return await prisma.food.findMany();
 }
 
 export async function deleteFoodById(foodId: number) {
-  return  await prisma.food.delete({
-    where: {id: foodId}
+  return await prisma.food.delete({
+    where: { id: foodId }
   });
 }
