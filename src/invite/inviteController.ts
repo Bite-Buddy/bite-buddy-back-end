eb31db9964cca541ab3fd292b16c2dc894e60250
Kingshuk Roy <kingshukroy98@gmail.com>
2023-09-15 13:06:46 +0900

    Develop delete invite controller

--- src/invite/inviteController.ts
+++ src/invite/inviteController.ts
@@ -9,9 +9,26 @@ export async function createInvite(req: Request, res: Response) {
         else {
             const recipientEmail = req.body.email;
             const kitchenId = req.body.kitchenId;
-            const invite = await inviteModel.createInvite(recipientEmail, kitchenId);
-            res.status(200).send(invite);
+            const invite = await inviteModel.createInvite(parseInt(kitchenId), recipientEmail);
+            res.status(200).send({
+                message: "Invite sent",
+                invite: invite,
+            });
         }
+    }
+    catch (error) {
+        res.status(500).send("Error: " + error);
+    }
+}
+
+export async function deleteInvite(req: Request, res: Response) {
+    try {
+        const inviteId = req.params.id;
+        const inviteResponse = await inviteModel.deleteInvite(parseInt(inviteId))
+        res.status(200).send({
+            message: "Invite deleted",
+            inviteResponse: inviteResponse,
+        })
     }
     catch (error) {
         res.status(500).send("Error: " + error);
