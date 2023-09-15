42edc4e31d3c7daf0946f9390db43cfab6b94bcb
Daniel Price <danieljamieprice@gmail.com>
2023-09-15 17:13:29 +0900

    Tidy code in line with style guide

--- src/invite/inviteModel.ts
+++ src/invite/inviteModel.ts
@@ -1,20 +1,14 @@
 import prisma from '../util/prisma-client';
-import * as userModel from "../user/userModel"
-import * as kitchenModel from "../kitchen/kitchenModel"
-
-// interface IInvite {
-//     recipientId: number,
-//     kitchenId: number
-// }
+import * as userModel from "../user/userModel";
 
 export async function createInvite(kitchenId: number, recipientEmail: string) {
     const recipient = await userModel.getByEmail(recipientEmail);
-    // const newInvite: IInvite = {recipientId: recipientId, kitchenId: kitchenId}
+
     if (recipient) {
         const recipientId = recipient.id;
         return await prisma.invite.create({
             data: {recipient_id: recipientId, kitchen_id: kitchenId}
-         })
+        });
     }
     else {
         return false;
@@ -23,22 +17,19 @@ export async function createInvite(kitchenId: number, recipientEmail: string) {
 
 export async function deleteInvite(inviteId: number) {
     return await prisma.invite.delete({
-        where: {id: inviteId}
+        where: { id: inviteId }
     })
 }
 
 export async function acceptInvite(inviteId: number) {
-    const inviteInfo = await prisma.invite.findUnique({ //Implement getById function within acceptInvite
-        where: {
-            id: inviteId
-        }
-    })
+    const inviteInfo = await prisma.invite.findUnique({
+        where: { id: inviteId }
+    });
     if (inviteInfo) {
-        await kitchenModel.addUserRelationship(inviteInfo.kitchen_id, inviteInfo.recipient_id);
         await userModel.addKitchenRelationship(inviteInfo.recipient_id, inviteInfo.kitchen_id);
         return await prisma.invite.delete({
             where: {id: inviteId}
-        })
+        });
     }
     else {
         return false;
