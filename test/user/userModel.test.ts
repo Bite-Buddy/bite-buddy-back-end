6fb2cf6f1c315288d2aaf5cd5636fe739b9c8aff
Daniel Price <danieljamieprice@gmail.com>
2023-09-17 16:17:52 +0900

    Write tests for delete and get users

--- test/user/userModel.test.ts
+++ test/user/userModel.test.ts
@@ -1,7 +1,8 @@
 import { prismaMock } from "../../singleton";
-import { createUser } from "../../src/user/userModel";
+import { createUser, deleteUserById, getUsers } from "../../src/user/userModel";
 
 describe('User Model - createUser', () => {
+
   it('should create a new user', async () => {
     const user = {
       id: 1,
@@ -9,14 +10,54 @@ describe('User Model - createUser', () => {
       email: "test@example.com",
     };
     
-    prismaMock.user.create.mockResolvedValue(user);
+    prismaMock.user.create.mockResolvedValue(user); //this is what we expect prisma.user.create to return
 
-    await expect(createUser(user)).resolves.toEqual({
+    await expect(createUser(user)).resolves.toEqual({ //this is what we expect our function to return
       id: 1,
       supabase_id: "123",
       email: "test@example.com",
     });
-
-    // prismaMock.user.create.mockImplementation()
   });
-})
+
+});
+
+describe("User Model - deleteUserById", () => {
+
+  it("should delete a user given an id", async () => {
+    const id = 1;
+    const user = {
+      id: 1,
+      supabase_id: "123",
+      email: "test@example.com",
+    };
+
+    prismaMock.user.delete.mockResolvedValue(user);
+
+    await expect(deleteUserById(id)).resolves.toEqual({
+      id: 1,
+      supabase_id: "123",
+      email: "test@example.com",
+    });
+  });
+
+});
+
+describe("User Model - getUsers", () => {
+
+  it('should return all existing users', async () => {
+    const user = [{
+      id: 1,
+      supabase_id: "123",
+      email: "test@example.com",
+    }];
+    
+    prismaMock.user.findMany.mockResolvedValue(user)
+
+    await expect(getUsers()).resolves.toEqual([{
+      id: 1,
+      supabase_id: "123",
+      email: "test@example.com",
+    }]);
+  });
+
+});
